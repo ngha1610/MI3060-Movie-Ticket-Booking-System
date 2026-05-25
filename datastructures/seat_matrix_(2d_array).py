f9b1f969abeb56e52b2fa09bd4239edf37282e34
ngha1610 <nguyetha16102010@gmail.com>
2026-05-25 21:07:17 +0700

    Update seat_matrix_(2d_array).py

--- datastructures/seat_matrix_(2d_array).py
+++ datastructures/seat_matrix_(2d_array).py
@@ -1,1 +1,25 @@
+# datastructures/seat_matrix.py
 
+class SeatMatrix:
+
+    def __init__(self,rows=5,cols=8):
+
+        self.rows=rows
+        self.cols=cols
+
+        self.seats=[["O" for _ in range(cols)]
+                    for _ in range(rows)]
+
+    def book_seat(self,row,col):
+
+        if self.seats[row][col]=="O":
+
+            self.seats[row][col]="X"
+            return True
+
+        return False
+
+
+    def get_layout(self):
+
+        return self.seats
