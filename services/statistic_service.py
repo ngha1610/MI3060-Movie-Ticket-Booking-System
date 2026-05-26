1ea0b4e39bf598068598a3ca8dc9cb455308acf6
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 12:50:11 +0700

    Update statistic_service.py

--- services/statistic_service.py
+++ services/statistic_service.py
@@ -1,1 +1,49 @@
+# services/statistic_service.py
 
+from collections import Counter
+
+from utils.csv_handler import CSVHandler
+
+
+class StatisticService:
+
+    def revenue(self):
+
+        tickets=CSVHandler.load_csv(
+            "data/tickets.csv"
+        )
+
+        total=sum(
+
+            int(ticket["price"])
+
+            for ticket in tickets
+
+        )
+
+        return total
+
+
+    def hot_movies(self):
+
+        tickets=CSVHandler.load_csv(
+            "data/tickets.csv"
+        )
+
+        counter=Counter(
+
+            ticket["movie"]
+
+            for ticket in tickets
+
+        )
+
+        return sorted(
+
+            counter.items(),
+
+            key=lambda x:x[1],
+
+            reverse=True
+
+        )
