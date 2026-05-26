5c5534fe9209808a7b9f4baf12b9084eb73328a5
ngha1610 <nguyetha16102010@gmail.com>
2026-05-26 13:00:14 +0700

    Update validator.py

--- utils/validator.py
+++ utils/validator.py
@@ -1,1 +1,51 @@
+import re
 
+
+class Validator:
+
+    @staticmethod
+    def username(name):
+
+        return len(name) >= 4
+
+
+    @staticmethod
+    def password(password):
+
+        if len(password) < 6:
+
+            return False
+
+        upper = re.search(
+            r"[A-Z]",
+            password
+        )
+
+        digit = re.search(
+            r"\d",
+            password
+        )
+
+        return upper and digit
+
+
+    @staticmethod
+    def role(role):
+
+        return role in [
+
+            "customer",
+            "admin"
+
+        ]
+
+
+    @staticmethod
+    def seat(seat):
+
+        pattern = r"^[A-Z]\d+$"
+
+        return re.match(
+            pattern,
+            seat
+        ) is not None
