358b79acdf80c9b4928595b71b09ba7ca0ed0b47
Hha-dhba <honganh0865@gmail.com>
2026-06-06 16:33:27 +0700

    Add files via upload

---
+++ data_structures/array.py
@@ -0,0 +1,19 @@
+# data_structures/custom_array.py
+
+class Array2D:
+    def __init__(self, rows, cols, default_value=0):
+        self.rows = rows
+        self.cols = cols
+        self.data = []
+     
+        total_elements = rows * cols
+        count = 0
+        while count < total_elements:
+            self.data.append(default_value)
+            count += 1
+
+    def get_val(self, r, c):  
+        return self.data[r * self.cols + c]
+
+    def set_val(self, r, c, value):
+        self.data[r * self.cols + c] = value
